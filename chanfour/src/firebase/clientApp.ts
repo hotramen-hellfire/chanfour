d07d9dd337e64afad99083f5a749cd6d09ad0ff3
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-14 19:20:27 +0530

    firebaseconfig'd

--- chanfour/src/firebase/clientApp.ts
+++ chanfour/src/firebase/clientApp.ts
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyBbnc8FgN47ZCdDN894Cp3RO5OZ6rf1gb4",
-    authDomain: "saitama-23e80.firebaseapp.com",
-    projectId: "saitama-23e80",
-    storageBucket: "saitama-23e80.appspot.com",
-    messagingSenderId: "843092703164",
-    appId: "1:843092703164:web:9f3891b182ae1543915db9",
-    measurementId: "G-GK81VGSV8W"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, firestore, auth, storage };
