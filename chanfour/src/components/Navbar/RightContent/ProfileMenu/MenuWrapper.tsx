200b56bd845aad5d2ab0d3ac84d811e2b0ab9938
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-16 23:30:42 +0530

    added username in menubar

--- chanfour/src/components/Navbar/RightContent/ProfileMenu/MenuWrapper.tsx
+++ chanfour/src/components/Navbar/RightContent/ProfileMenu/MenuWrapper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
@@ -13,7 +13,7 @@ import {
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
-import { authentication } from "../../../../firebase/clientApp";
+import { authentication, firestore } from "../../../../firebase/clientApp";
 
 
 import { FaRedditSquare } from "react-icons/fa";
@@ -21,18 +21,32 @@ import { VscAccount } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
 import UserList from "./UserList";
 import NoUserList from "./NoUserList";
+import { doc, getDoc } from "firebase/firestore";
+import { TiSpiral } from "react-icons/ti";
 
 type MenuWrapperProps = {};
 
 const MenuWrapper: React.FC<MenuWrapperProps> = () => {
     const [authModal, setModalState] = useRecoilState(authModalState);
     const [user] = useAuthState(authentication);
+    const [UName, setUName] = useState('notfetched');
+    const getUName = async () => {
+        if (!user) { return; }//will never be invoked
+        setUName("reached");
+        var uid = user.email!.split("@")[0];
+        const userDocRef = doc(firestore, 'userByID', uid);
+        const userDoc = await getDoc(userDocRef);
+        setUName(userDoc.data()!['UName'] as string);
+        return;
+    }
+    if (UName === 'notfetched' && user) getUName();
     return (
         <Menu>
             <MenuButton
                 cursor="pointer"
                 padding="0px 6px"
                 borderRadius="4px"
+                color={"white"}
                 _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
             >
                 <Flex alignItems="center">
@@ -42,22 +56,22 @@ const MenuWrapper: React.FC<MenuWrapperProps> = () => {
                                 <Icon
                                     fontSize={24}
                                     mr={1}
-                                    color="gray.300"
+                                    color="white"
                                     as={FaRedditSquare}
                                 />
                                 <Box
                                     display={{ base: "none", lg: "flex" }}
                                     flexDirection="column"
                                     fontSize="8pt"
-                                    alignItems="flex-start"
+                                    alignItems="center"
                                     mr={8}
                                 >
                                     <Text fontWeight={700}>
-                                        {user?.displayName || user?.email?.split("@")[0]}
+                                        {UName}
                                     </Text>
                                     <Flex alignItems="center">
-                                        <Icon as={IoSparkles} color="brand.100" mr={1} />
-                                        <Text color="gray.400">1 karma</Text>
+                                        <Icon as={TiSpiral} color="white" mr={1} fontSize={20} />
+                                        <Text color="gray.100">1 karma</Text>
                                     </Flex>
                                 </Box>
                             </>
