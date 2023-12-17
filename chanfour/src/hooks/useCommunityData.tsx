5801fc92516e9a4e427d7be2da32f050204385fc
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-17 23:02:50 +0530

    implemented fetching community snippets

--- chanfour/src/hooks/useCommunityData.tsx
+++ chanfour/src/hooks/useCommunityData.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Community, communityState } from '../components/atoms/communitiesAtom';
 import { useRecoilState } from 'recoil';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { authentication, firestore } from '../firebase/clientApp';
+import { getDocs, collection } from 'firebase/firestore';
 
 const useCommunityData = () => {
     const [commmunityStateValue, setCommunityStateValue] = useRecoilState(communityState)
     const [leading, setLoading] = useState(true);
     const [error, setError] = useState('');
+    const [user] = useAuthState(authentication)
+    var uid = "";
+    if (user && user.email) uid = user?.email.split('.')[0];
     const onJoinOrLeaveCommunity = (communityData: Community, isJoined: boolean) => {
         //is the user signed in
         //if not open auth modal state
@@ -20,7 +26,14 @@ const useCommunityData = () => {
         }
     }
     const getMySnippets = async () => {
+        setLoading(true);
+        if (!uid) {
+            //do something and return
+            return;
+        }
         try {
+            const snippetDocs = await getDocs(collection(firestore, '/userByID/' + uid + '/communitySnippets'));
+            const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }))
 
         } catch (error: any) {
             console.log('getMySnippets error: ', error);
@@ -29,6 +42,12 @@ const useCommunityData = () => {
     }
     const joinCommunity = (communityData: Community) => { };
     const leaveCommunity = (communityID: string) => { };
+
+    useEffect(() => {
+        if (!user) return;
+        getMySnippets();
+    }, [user])
+
     return {
         commmunityStateValue,
         onJoinOrLeaveCommunity
