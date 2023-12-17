5a263f9deb8c20c621a2f760883a499ed5677ff7
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-17 16:27:15 +0530

    using imagebanks

---
+++ chanfour/src/pages/r/[communityName]/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type CommunityPageProps = {
+
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = () => {
+
+    return <div>CommunityPg</div>
+}
+export default CommunityPage;
