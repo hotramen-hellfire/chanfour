abdc2d22c9510620f43faad88b5eac36b7e5a36c
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-17 15:56:19 +0530

    rebasing :(

--- chanfour/src/firebase/errors.ts
+++
@@ -1,6 +0,0 @@
-export const FIREBASE_ERRORS = {
-    "Firebase: Error (auth/email-already-in-use).": "email already in use :(",
-    "Firebase: Error (auth/user-not-found).": "incorrect email or passphrase",
-    "Firebase: Error (auth/wrong-password).": "incorrect email or passphrase",
-    "Firebase: Error (auth/invalid-credential).": "incorrect email or passphrase"
-};
