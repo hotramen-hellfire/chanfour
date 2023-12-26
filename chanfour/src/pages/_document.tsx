85e0dbda54460cc8d3c2e197d0a327a69b7cd23f
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-26 12:50:43 +0530

    updated title and head

--- chanfour/src/pages/_document.tsx
+++ chanfour/src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <title>
+          SAITAMA
+        </title>
+        <link rel="icon" href="/leaf.png" />
+      </Head>
       <body >
         <Main />
         <NextScript />
