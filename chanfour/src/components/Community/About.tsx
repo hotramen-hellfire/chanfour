2a4bd6ab44780ba2403bc5f0f56fbe8ddb3e5434
hotramen-hellfire <22b0956@iitb.ac.in>
2023-12-27 14:49:18 +0530

    implemented tags on community page

--- chanfour/src/components/Community/About.tsx
+++ chanfour/src/components/Community/About.tsx
@@ -3,6 +3,8 @@ import { Community, communityState } from '../Atoms/communitiesAtom';
 import { Code, Flex, Stack, Text } from '@chakra-ui/react';
 import moment from 'moment';
 import { useRecoilState, useRecoilValue } from 'recoil';
+import { firestore } from '@/src/firebase/clientApp';
+import { doc, getDoc } from 'firebase/firestore';
 
 type AboutProps = {
     communityData: Community
@@ -11,9 +13,45 @@ type AboutProps = {
 const About: React.FC<AboutProps> = ({ communityData }) => {
     const communityStateValue = useRecoilValue(communityState);
     const [numPosts, setNumPosts] = useState(communityData.numberOfPosts);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [tags, setTags] = useState<string[]>([]);
+    const [tagId, setTagId] = useState("")
+    const colors = ["yellow", 'red', 'blue', 'green', 'pink', 'purple']
     useEffect(() => {
         if (communityStateValue.currentCommunity) setNumPosts(communityStateValue.currentCommunity!.numberOfPosts);
     }, [communityStateValue])
+    useEffect(() => {
+        // setSelectedCommunity("");
+        const getTags = async () => {
+            setLoading(true);
+            try {
+                const docRef = doc(firestore, 'meta', 'tags')
+                const document = await getDoc(docRef);
+                if (!document.exists) throw Error("");
+                const value = { ...document.data() }
+                if (communityStateValue.currentCommunity) {
+                    if (value[communityStateValue.currentCommunity.communityID]) {
+                        setTags((value[communityStateValue.currentCommunity.communityID] as string).split(',') as string[]);
+                        setTagId(communityStateValue.currentCommunity.communityID);
+                    }
+                    else {
+                        setTags([]);
+                        setTagId(communityStateValue.currentCommunity.communityID);
+                    }
+                }
+                else {
+                    setTags([]);
+                }
+            } catch (error: any) {
+                setError(error.message);
+                console.log("getTags error: ", error.message);
+            }
+            setLoading(false);
+        }
+        if (communityStateValue.currentCommunity) if (tags.length < 1 || tagId !== communityStateValue.currentCommunity.communityID) getTags();
+    }, [communityStateValue.currentCommunity])
+
     return (
         <>
             <Flex border='2px solid purple'
@@ -48,6 +86,30 @@ const About: React.FC<AboutProps> = ({ communityData }) => {
                 <Flex width={'100%'} padding="2px 4px 4px 2px" fontSize={14}>
                     {communityData.description}
                 </Flex>
+                {tags &&
+                    <Flex
+                        width={'100%'}
+                        // height={'200px'}
+                        // border={'1px solid red'}
+                        flexDirection={'row'}
+                        p={1}
+                        flexWrap={'wrap'}
+                    >
+
+                        {tags.map(item =>
+                            <Code
+                                height={'20px'}
+                                colorScheme={colors[Math.floor(Math.random() * colors.length)]}
+                                mr={1}
+                                mb={1}
+                                border={'1px solid black'}
+                            >
+                                {item}
+                            </Code>
+                        )}
+                    </Flex>
+
+                }
                 <Flex width={'100%'} padding="2px 4px 4px 2px" fontSize={12} color={'gray'} justify={'left'} flexDirection={'column'}>
                     created by {communityData.creatorID}
                     {communityData.createdAt && <Text>{moment(new Date(communityData.createdAt.seconds * 1000)).format("MMM DD, YYYY")}</Text>}
